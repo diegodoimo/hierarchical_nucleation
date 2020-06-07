0b1d32a8bfd3d404d2f818b696e61e205115a35f
Aldo <ealdoglielmo@gmail.com>
2020-06-07 14:38:22 +0200

    first working version of jupyter notebook

--- utils.py
+++ utils.py
@@ -1,7 +1,6 @@
 import numpy as np
 
 np.set_printoptions(precision=2)
-from sklearn.neighbors import NearestNeighbors
 import time
 from scipy.optimize import curve_fit
 from scipy.special import gammaln
@@ -15,6 +14,40 @@ np.set_printoptions(precision=2)
 import os
 
 os.getcwd()
+
+
+def double_centering(M):
+    N = M.shape[0]
+
+    Mc = - 0.5 * (M - 1. / N * np.sum(M, axis=0)[None, :] - 1. / N * np.sum(M, axis=1)[:,
+                                                                     None] + 1. / N ** 2 * np.sum(
+        M))
+
+    return Mc
+
+
+def kernel_PCA(layer):
+    # obtain distance matrix from density peaks and saddle points
+    distances = max(layer.Rho) - layer.out_bord
+    distances[np.diag_indices_from(distances)] = 0.
+
+    if distances.shape[0] < 2:
+        return np.array([[0, 0]])
+
+    else:
+        # compute average distance to set lengthscae of gaussian kernel
+        md = np.mean(distances[np.triu_indices_from(distances)])
+        sig = np.mean(md)
+
+        # compute kernel matrix
+        ker = np.exp(- distances ** 2 / (0.5 * sig ** 2))
+        ker = double_centering(-ker)
+
+        e, v = np.linalg.eigh(ker)
+        s = np.argsort(e)[::-1]
+        out = v[:, s[:2]]
+
+        return out
 
 
 class Data:
@@ -35,16 +68,12 @@ class Data:
         self.njobs = njobs
         self.gt_labels = labels
 
-
     def compute_id(self, decimation=1, fraction=0.9, algorithm='base'):
-
-        if self.distances is None: self.compute_distances()
-
         assert (0. < decimation and decimation <= 1.)
 
-        #self.id_estimated_ml, self.id_estimated_ml_std = return_id(self.distances, decimation)
+        # self.id_estimated_ml, self.id_estimated_ml_std = return_id(self.distances, decimation)
 
-        #Nele = len(distances)
+        # Nele = len(distances)
         # remove highest mu values
         Nele_eff = int(self.Nele * fraction)
         mus = np.log(self.distances[:, 2] / self.distances[:, 1])
@@ -69,10 +98,9 @@ class Data:
         self.id_selected = int(np.around(id, decimals=0))
 
         if self.verb:
-            #print('ID estimated from ML is {:f} +- {:f}'.format(self.id_estimated_ml, self.id_estimated_ml_std))
-            #print(f'Selecting ID of {self.id_selected}')
+            # print('ID estimated from ML is {:f} +- {:f}'.format(self.id_estimated_ml, self.id_estimated_ml_std))
+            # print(f'Selecting ID of {self.id_selected}')
             print(f'ID estimation finished: selecting ID of {self.id_selected}')
-
 
     def compute_density_kNN(self, k=30):
         assert (self.id_selected is not None)
@@ -106,7 +134,6 @@ class Data:
         self.kstar = kstar
 
         if self.verb: print('k-NN density estimation finished')
-
 
     def compute_clustering(self, Z=1.65, halo=False):
         assert (self.Rho is not None)
@@ -145,7 +172,6 @@ class Data:
             print('Clustering finished, {} clusters found'.format(self.Nclus_m))
             print('total time is, {}'.format(secf - seci))
 
-
     def return_rank_distribution(self):
         neigh_identities = self.dist_indices[:, 1:] == self.gt_labels[:, None]
 
@@ -153,7 +179,6 @@ class Data:
         different_label = np.sum(~neigh_identities, axis=1) / self.Nele
 
         return same_label, different_label
-
 
     def return_label_overlap_all(self, k=30):
         assert (self.distances is not None)
@@ -165,7 +190,6 @@ class Data:
             overlaps.append(sum(self.gt_labels[neigh_idx_i] == self.gt_labels[i]) / k)
 
         return overlaps
-
 
     def return_label_overlap_mean(self, k=30):
         assert (self.distances is not None)
@@ -179,3 +203,104 @@ class Data:
         overlap = overlap / self.Nele
 
         return overlap
+
+
+class Data_sets:
+
+    def __init__(self, distances_list=(), labels_list=(),
+                 maxk_list=[None], verbose=False, njobs=1):
+
+        self.Nsets = len(distances_list)
+
+        if len(maxk_list) == 1:
+            maxk_list = [maxk_list[0]] * self.Nsets
+
+        assert (len(labels_list) == 0 or len(labels_list) == self.Nsets)
+
+        self.data_sets = []
+
+        for i in range(self.Nsets):
+            dists = distances_list[i]
+            maxk = maxk_list[i]
+
+            labels = labels_list[i]
+            data = Data(distances=dists, labels=labels, maxk=maxk, verbose=verbose, njobs=njobs)
+
+            self.data_sets.append(data)
+
+        self.verbose = verbose
+        self.njobs = njobs
+        self.ids = None  # ids
+        self.ov_gt = None  # overlap ground truth (classes)
+        self.ov_out = None  # overlap output neighborhoods
+        self.ov_ll = None  # overlap ll neighbourhoods
+
+    def add_one_dataset(self, distances=None, labels=None, maxk=None):
+
+        data = Data(distances=distances, labels=labels,
+                    maxk=maxk, verbose=self.verbose, njobs=self.njobs)
+
+        self.data_sets.append(data)
+        self.Nsets += 1
+
+    def compute_id(self, decimation=1, fraction=0.9):
+
+        # print(f'computing id: fraction = {fraction}, decimation = {decimation}, range = 2'.format)
+        for i, d in enumerate(self.data_sets):
+            print('computing id of layer ', i)
+            d.compute_id(decimation=decimation, fraction=fraction)
+            # print('id computation finished')
+
+        self.ids = [d.id_selected for d in self.data_sets]
+
+    def compute_density_kNN(self, k):
+        for i, d in enumerate(self.data_sets):
+            print('computing kNN density for dataset ', i)
+            d.compute_density_kNN(k=k)
+
+    def compute_clustering(self, Z=1.65, halo=True):
+
+        print('Z = {}, halo = {}'.format(Z, halo))
+        for i, d in enumerate(self.data_sets):
+            print('computing clustering for dataset ', i)
+            d.compute_clustering(Z=Z, halo=halo)
+        print('Z = {}, halo = {}'.format(Z, halo))
+
+    def return_label_overlap_mean(self, k=30):
+
+        overlap_means = []
+
+        for i, d in enumerate(self.data_sets):
+            print('computing overlap for dataset ', i)
+            overlap_means.append(d.return_label_overlap_mean(k=k))
+
+        return overlap_means
+
+    def return_label_overlap_all(self, k=30):
+
+        overlap_alls = []
+
+        for i, d in enumerate(self.data_sets):
+            print('computing overlap for dataset ', i)
+            overlap_alls.append(d.return_label_overlap_all(k=k))
+
+        return overlap_alls
+
+    def return_overlap_mean_btw_layers(self, i1, i2, k=30):
+
+        ranks_layer1 = self.data_sets[i1].dist_indices
+        ranks_layer2 = self.data_sets[i2].dist_indices
+
+        Nimg_tot = ranks_layer1.shape[0]
+        cs = []
+        for i in range(Nimg_tot):
+            cs.append(len(set(ranks_layer1[i, 1:k + 1]) & set(ranks_layer2[i, 1:k + 1])))
+
+        return np.mean(cs) / k
+
+    def return_overlap_mean_ll(self, step=1, k=30):
+        ov_ll = []
+        for i in range(self.Nsets - step):
+            print('computing overlap for datasets ', i, i + step)
+            ov_ll.append(self.return_overlap_mean_btw_layers(i, i + step, k))
+        return ov_ll
