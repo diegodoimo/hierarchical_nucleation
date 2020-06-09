75d99ecaccdbcc18f3d83454ddf4cd5e59e8d06e
Aldo <ealdoglielmo@gmail.com>
2020-06-09 16:22:30 +0200

    cleanup of jupyter and folder structure

--- setup_cython.py
+++ setup_cython.py
@@ -2,4 +2,4 @@ from distutils.core import setup
 from Cython.Build import cythonize
 import numpy
 
-setup(ext_modules=cythonize('cython_clustering.pyx', annotate = True), include_dirs=[numpy.get_include()])
+setup(ext_modules=cythonize('src/cython_clustering.pyx', annotate = True), include_dirs=[numpy.get_include()])
